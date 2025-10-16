c8e5c37a7094dd5ef41d013d77d830ff4b285de6
Ali Bondar <bondarali1380@gmail.com>
2025-10-16 15:35:27 +0330

    fix: update get version description

--- src/main/java/com/bondar/urlshortener/web/rest/ShortUrlResource.java
+++ src/main/java/com/bondar/urlshortener/web/rest/ShortUrlResource.java
@@ -170,7 +170,7 @@ public class ShortUrlResource {
     public ResponseEntity<Map<String, String>> getVersion() {
         Map<String, String> versionInfo = new HashMap<>();
         versionInfo.put("version", "1.0.0");
-        versionInfo.put("description", "URL Shortener Service(Built by JHipster");
+        versionInfo.put("description", "URL Shortener Service(Built by JHipster)");
         versionInfo.put("author", "Ali Bondar");
         versionInfo.put("date", LocalDateTime.now().toString());
         return ResponseEntity.ok(versionInfo);
